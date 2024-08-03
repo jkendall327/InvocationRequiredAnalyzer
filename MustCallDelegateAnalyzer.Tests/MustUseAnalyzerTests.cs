f714496f1e18e1aef6be700165b4d36443720828
jackkendall <jkendall3096@gmail.com>
2024-08-03 09:25:53 +0100

    Add various basic tests

--- MustCallDelegateAnalyzer.Tests/MustUseAnalyzerTests.cs
+++ MustCallDelegateAnalyzer.Tests/MustUseAnalyzerTests.cs
@@ -6,13 +6,14 @@ namespace MustCallDelegateAnalyzer.Tests;
 
 public class MustUseAnalyzerTests
 {
+    private readonly DiagnosticResult _result = new DiagnosticResult(MustUseTypeAnalyzer.Rule)
+        .WithArguments("callback");
+    
     [Fact]
     public async Task AnalyzerTriggers_WhenMustUseType_IsArgToEmptyMethod()
     {
         var context = new CSharpAnalyzerTest<MustUseTypeAnalyzer, DefaultVerifier>
         {
-            ReferenceAssemblies = ReferenceAssemblies.Net.Net80,
-            
             TestCode = """
                        using System;
                        
@@ -32,9 +33,129 @@ public class MustUseAnalyzerTests
                        """,
         };
 
-        var result = new DiagnosticResult(MustUseTypeAnalyzer.Rule)
-            .WithArguments("callback")
-            .WithSpan(11, 28, 11, 54);
+        var result = _result.WithSpan(11, 28, 11, 54);
+        
+        context.ExpectedDiagnostics.Add(result);
+        
+        await context.RunAsync();
+    }
+    
+    [Fact]
+    public async Task AnalyzerDoesNotTrigger_WhenMustUseType_IsInvokedAsMethod()
+    {
+        var context = new CSharpAnalyzerTest<MustUseTypeAnalyzer, DefaultVerifier>
+        {
+            TestCode = """
+                       using System;
+
+                       [AttributeUsage(AttributeTargets.Delegate)]
+                       public class MustUseAttribute : Attribute { }
+
+                       [MustUse]
+                       public delegate void ImportantCallback();
+
+                       public class TestClass
+                       {
+                           public void TestMethod(ImportantCallback callback)
+                           {
+                               callback();
+                           }
+                       }
+                       """,
+        };
+
+        await context.RunAsync();
+    }
+    
+    [Fact]
+    public async Task AnalyzerDoesNotTrigger_WhenMustUseType_IsInvokedExplicitly()
+    {
+        var context = new CSharpAnalyzerTest<MustUseTypeAnalyzer, DefaultVerifier>
+        {
+            TestCode = """
+                       using System;
+
+                       [AttributeUsage(AttributeTargets.Delegate)]
+                       public class MustUseAttribute : Attribute { }
+
+                       [MustUse]
+                       public delegate void ImportantCallback();
+
+                       public class TestClass
+                       {
+                           public void TestMethod(ImportantCallback callback)
+                           {
+                               callback.Invoke();
+                           }
+                       }
+                       """,
+        };
+
+        await context.RunAsync();
+    }
+    
+    [Fact]
+    public async Task AnalyzerDoesNotTrigger_WhenMustUseType_IsPassedToOtherMethod_AndThenInvoked()
+    {
+        var context = new CSharpAnalyzerTest<MustUseTypeAnalyzer, DefaultVerifier>
+        {
+            TestCode = """
+                       using System;
+
+                       [AttributeUsage(AttributeTargets.Delegate)]
+                       public class MustUseAttribute : Attribute { }
+
+                       [MustUse]
+                       public delegate void ImportantCallback();
+
+                       public class TestClass
+                       {
+                           public void PassCallback(ImportantCallback callback)
+                           {
+                               InvokeCallback(callback);
+                           }
+                           
+                           public void InvokeCallback(ImportantCallback callback)
+                           {
+                               callback.Invoke();
+                           }
+                       }
+                       """,
+        };
+
+        await context.RunAsync();
+    }
+    
+    [Fact]
+    public async Task AnalyzerTriggers_WhenMustUseType_IsPassedToOtherMethod_AndThenNotInvoked()
+    {
+        var context = new CSharpAnalyzerTest<MustUseTypeAnalyzer, DefaultVerifier>
+        {
+            TestCode = """
+                       using System;
+
+                       [AttributeUsage(AttributeTargets.Delegate)]
+                       public class MustUseAttribute : Attribute { }
+
+                       [MustUse]
+                       public delegate void ImportantCallback();
+
+                       public class TestClass
+                       {
+                           public void PassCallback(ImportantCallback callback)
+                           {
+                               InvokeCallback(callback);
+                           }
+                           
+                           public void InvokeCallback(ImportantCallback callback)
+                           {
+                               
+                           }
+                       }
+                       """,
+        };
+
+        var result = _result.WithSpan(16, 32, 16, 58);
         
         context.ExpectedDiagnostics.Add(result);
         
